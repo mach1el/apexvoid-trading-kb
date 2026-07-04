86b56386a2a5d88b251676fe122c5824284c6971
mach1el <michaeldang.general@gmail.com>
2026-07-04 19:00:07 +0700

    fix: unused navigation import

--- src/components/PageNavigation.tsx
+++ src/components/PageNavigation.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { flattenNav, navigation } from '../content/nav';
+import { flattenNav } from '../content/nav';
 
 export function PageNavigation() {
   const location = useLocation();
